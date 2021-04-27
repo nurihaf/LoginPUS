af1baed0f1994a2adad0d37caa5b2c001bd341d6
nurihaf <a.nurhafizah98@gmail.com>
2021-04-27 14:28:56 +0800

    2nd

---
+++ app/src/main/java/com/example/loginpus/HomeActivity.java
@@ -0,0 +1,38 @@
+package com.example.loginpus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btnLogout;
+    Button btnUpdate;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btnLogout = findViewById(R.id.logout);
+        btnUpdate = findViewById(R.id.button2);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intToMain);
+            }
+        });
+
+    }
+
+    public void btnUpdatelocation(View view) {
+    }
+}
